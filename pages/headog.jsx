5b1549d9626785ed452c7267eecadaaceab1e289
Xeli0701 <yamato232@live.jp>
2021-08-04 23:28:06 +0900

    component分割、ロゴ作成など

---
+++ pages/headog.jsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+export const Headog = () => {
+  return (
+    <Head>
+    <title>HSM</title>
+    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta property="og:url" content="" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="HSM ver0.1" />
+    <meta property="og:description" content="俳句を自動で作る。" />
+    <meta property="og:site_name" content="HSM ver0.1" />
+    <meta property="og:image" content="" />
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@Xeli0701" />
+    <link rel="icon" href="/favicon.ico" />
+    </Head>
+  )
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,8 @@
-import Head from 'next/head'
 import Image from 'next/image'
 import Link from "next/link";
 import styles from '../styles/Home.module.css'
 import { Header } from "./header";
+import { Headog } from "./headog";
 import { Footer } from "./footer";
 import { useState, useRef } from "react";
 
@@ -11,9 +11,10 @@ export default function Home() {
   const modalRef = useRef()
 
   return (
-    <div class="container">
+    <div className="container mx-auto">
+      <Headog />
       <Header />
-      <div class="container">
+      <div className="container">
         <div className="text-red-400">
           Hello World!
         </div>
